1949a49d8f54ccb71e807b1094edb59c5dc36088
Timur Kheyfets <to@timur.io>
2020-08-19 20:56:35 -0400

    added support for event source

---
+++ app/controllers/event_source_controller.rb
@@ -0,0 +1,42 @@
+class EventSourceController < ApplicationController
+  include ActionController::Live
+
+  NUMBERS_COUNT = 100_000
+  FLUSH_EVERY = 500
+
+  def index
+    headers["Content-Type"] = "text/event-stream"
+    unique = (Time.current.to_f * 1_000).to_i
+    sse = SSE.new(response.stream, retry: 300)
+    sse.write({ chunkSize: FLUSH_EVERY, total: NUMBERS_COUNT }.to_json, event: 'header', id: unique )
+    bufferArray = []
+    unique += 1
+    NUMBERS_COUNT.times do |i|
+        bufferArray.push(random_number_object_lazy(i))
+        if i % FLUSH_EVERY == 0 && i != 0
+          sse.write(bufferArray.to_json, event: 'data', id: unique)	
+          bufferArray.clear
+          unique += 1
+        end 
+
+        if i % 5_000 == 0
+          sleep 0.1
+        end
+    end
+    unique += 1
+    sse.write(nil, event: 'end', id: unique )
+  ensure	
+    sse.close
+  end
+
+  private
+
+  def random_number_object_lazy(index)
+    random_number = SecureRandom.random_number(100)
+    {	
+      index:  index,
+      time:   Time.current.xmlschema(3),
+      number: random_number
+    }    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get '/event-source', to: 'event_source#index'	
   root 'stream#index'
 end
